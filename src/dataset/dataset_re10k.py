ba589b516fd86c835f6d761068edc58cdd8d8aca
dcr <1456882404@qq.com>
2024-11-21 13:00:55 +0800

    finish 1.0 dataloader

--- src/dataset/dataset_re10k.py
+++ src/dataset/dataset_re10k.py
@@ -12,6 +12,8 @@ from jaxtyping import Float, UInt8
 from PIL import Image
 
 from .dataset import DatasetCfgCommon
+from .view_sampler.view_sampler_scene import ViewSamplerSceneCfg
+from utils.geometry_utils import get_fov
 
 @dataclass
 class DatasetRe10kCfg:
@@ -23,6 +25,10 @@ class DatasetRe10kCfg:
     data_params: str
     root: str
     small_test: bool
+    max_fov: float
+    make_baseline_1: bool
+    
+    view_sampler: ViewSamplerSceneCfg
 
 class Re10kDataset(IterableDataset):
     cfg: DatasetRe10kCfg
@@ -41,7 +47,7 @@ class Re10kDataset(IterableDataset):
         
         assert os.path.exists(self.cfg.root)
         self.chunks = []
-        if stage in ["train", "val"]:
+        if self.stage in ["train", "val"]:
             assert os.path.basename(self.cfg.root) == "train"
         else:
             assert os.path.basename(self.cfg.root) == "test"
@@ -51,6 +57,100 @@ class Re10kDataset(IterableDataset):
             chunk_path = os.path.join(self.cfg.root, chunk)
             self.chunks.append(chunk_path)
 
+    def shuffle(self, lst: list) -> list:
+        indices = torch.randperm(len(lst))
+        return [lst[x] for x in indices]
+    
+    def __iter__(self):
+        if self.stage in ["train", "val"]:
+            self.chunks = self.shuffle(self.chunks)
+        
+        for chunk_path in self.chunks:
+            chunk = torch.load(chunk_path)
+            if self.stage in ["train", "val"]:
+                chunk = self.shuffle(chunk)
+            
+            for example in chunk:
+                extrinsics, intrinsics = self.convert_poses(example["cameras"])
+                scene = example["key"]
+                
+                # sample images from patch
+                try:
+                    context_indices, target_indices = self.view_sampler.sample(
+                        scene,
+                        extrinsics,
+                        intrinsics
+                    )
+                except ValueError:
+                    continue
+                
+                # skip the example if the field of view is too large
+                if (get_fov(intrinsics).rad2deg() > self.cfg.max_fov).any():
+                    continue
+                
+                # load images
+                try:
+                    context_images = [
+                        example["images"][index.item()] for index in context_indices
+                    ]
+                    context_images = self.convert_images(context_images)
+                    target_images = [
+                        example["images"][index.item()] for index in target_indices
+                    ]
+                    target_images = self.convert_images(target_images)
+                except IndexError:
+                    continue
+                
+                # skip when image do not have right shape
+                context_image_invalid = context_images.shape[1:] != (3, 360, 640)
+                target_image_invalid = target_images.shape[1:] != (3, 360, 640)
+                if context_image_invalid or target_image_invalid:
+                    print(
+                        f"Skipped bad example {example['key']}. Context shape was "
+                        f"{context_images.shape} and target shape was "
+                        f"{target_images.shape}."
+                    )
+                    continue
+                
+                # Resize the world to make the baseline 1
+                context_extrinsics = extrinsics[context_indices]
+                if context_extrinsics.shape[0] == 2 and self.cfg.make_baseline_1:
+                    a, b = context_extrinsics[:, :3, 3]
+                    scale = (a - b).norm()
+                    if scale < self.cfg.baseline_epsilon:
+                        print(
+                            f"Skipped {scene} because of insufficient baseline "
+                            f"{scale:.6f}"
+                        )
+                        continue
+                    extrinsics[:, :3, 3] /= scale
+                else:
+                    scale = 1
+                
+                # TODO convert the example to ray and resize the image
+                example = {
+                    "context": {
+                        "extrinsics": extrinsics[context_indices],
+                        "intrinsics": intrinsics[context_indices],
+                        "image": context_images,
+                        "near": self.get_bound("near", len(context_indices)) / scale,
+                        "far": self.get_bound("far", len(context_indices)) / scale,
+                        "index": context_indices,
+                    },
+                    "target": {
+                        "extrinsics": extrinsics[target_indices],
+                        "intrinsics": intrinsics[target_indices],
+                        "image": target_images,
+                        "near": self.get_bound("near", len(target_indices)) / scale,
+                        "far": self.get_bound("far", len(target_indices)) / scale,
+                        "index": target_indices,
+                    },
+                    "scene": scene,
+                }
+            pass
+    
+    def __len__(self):
+        return len(self.chunks)
     
     def convert_poses(
         self,
---
+++ src/utils/geometry_utils.py
@@ -0,0 +1,26 @@
+import os
+
+import torch
+from torch import Tensor
+
+from jaxtyping import Bool, Float, Int64
+from einops import einsum, rearrange, reduce, repeat
+import cv2 as cv
+import numpy as np
+
+def get_fov(intrinsics: Float[Tensor, "batch 3 3"]) -> Float[Tensor, "batch 2"]:
+    intrinsics_inv = intrinsics.inverse()
+
+    def process_vector(vector):
+        vector = torch.tensor(vector, dtype=torch.float32, device=intrinsics.device)
+        vector = einsum(intrinsics_inv, vector, "b i j, j -> b i")
+        return vector / vector.norm(dim=-1, keepdim=True)
+
+    left = process_vector([0, 0.5, 1])
+    right = process_vector([1, 0.5, 1])
+    top = process_vector([0.5, 0, 1])
+    bottom = process_vector([0.5, 1, 1])
+    fov_x = (left * right).sum(dim=-1).acos()
+    fov_y = (top * bottom).sum(dim=-1).acos()
+    return torch.stack((fov_x, fov_y), dim=-1)
+
